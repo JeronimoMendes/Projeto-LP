22de13777f13d8d8559197a378e9e1dc07d70354
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-04-28 17:06:00 +0100

    Start repo

---
+++ projeto.pl
@@ -0,0 +1,8 @@
+/*  
+**  File: projeto.pl
+**  Author: Jeronimo Mendes 99086 LEIC-T 
+**  Description: LP Project. Kakuro puzzle solver.
+*/
+
+% Imports commun code and puzzles
+consult(codigo_comum).
