3205b3c6716bc89b8ff212fc2da08d8819493db5
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-05-01 03:10:07 +0100

    Add combincoes_soma/4

--- projeto.pl
+++ projeto.pl
@@ -5,4 +5,27 @@
 */
 
 % Imports commun code and puzzles
-consult(codigo_comum).
+:-consult(codigo_comum).
+
+/*
+**
+**  MAIN PREDICATES
+**
+*/
+
+combinacoes_soma(N, Els, Soma, Combs):-
+	findall(Combs,combinacao(N,Els,Combs),X),
+	include(list_sum(Soma), X, Combs).
+
+
+/*
+**
+**  AUXILIAR PREDICATES
+**
+*/
+
+% Sum of all elements of a list.
+list_sum(0,[]).
+list_sum(Sum, [H|T]) :-
+   sum_list(T, Rest),
+   Sum is H + Rest.
