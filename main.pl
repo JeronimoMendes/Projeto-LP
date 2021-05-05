e3f400003b95af2c99464a61f3e50486b2ba8455
JeronimoMendes <jeronimo.m.mendes@gmail.com>
2021-05-05 01:19:55 +0100

    Add permutacoes_soma/4

--- main.pl
+++ main.pl
@@ -15,8 +15,15 @@
 
 combinacoes_soma(N, Els, Soma, Combs):-
 	findall(Combs,combinacao(N,Els,Combs),X),
-	include(list_sum(Soma), X, Combs).
+	include(list_sum_helper(Soma), X, Combs).
 
+
+permutacoes_soma(N, Els, Soma, Perms):-
+   combinacoes_soma(N, Els, Soma, X),
+   findall(Y, maplist(permutation, X, Y), Z),
+   append(Z, A),
+   list_to_set(A, Perms).
+      
 
 /*
 **
@@ -24,8 +31,12 @@ combinacoes_soma(N, Els, Soma, Combs):-
 **
 */
 
-% Sum of all elements of a list.
-list_sum(0,[]).
-list_sum(Sum, [H|T]) :-
-   sum_list(T, Rest),
-   Sum is H + Rest.
+% Helper predicates. They change the order of arguments so that they
+% can be used in excluce/3, include/3...
+list_sum_helper(Sum, List) :- sum_list(List, Sum).
+
+lenght_helper(Length, List) :- length(List, Length).
+
+append_helper(L3, L2, L1) :- append(L1, L2, L3).
+
+
